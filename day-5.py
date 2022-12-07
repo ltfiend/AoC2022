3bd5d4e4756f73305a2f4855fc7bc1cfe93bd4c9
Peter DeVries <peter.s.devries2.ctr@us.adm.mil>
2022-12-06 23:24:07 -0500

    Day 5 Part 2 done, Messy

--- day-5.py
+++ day-5.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 import re
-import numpy as np
-
-
-def printTable3(data):
-    for d in data:
-        print(d)
-    
-    test = np.array(data)
-    print(np.shape(test))
-
-    for num in test:
-        print(num)
-    # print(f"test {test[0]}")
-    print(test[0, 0])
 
 
 def printTable2(data):
@@ -21,16 +7,22 @@ def printTable2(data):
         print(x)
         print(data[x])
 
+
 def doMovement(line, column):
-    # print(column)
+    print(line)
+    crane = []
     moveCommand = line.split(" ")
     amount = int(moveCommand[1])
     fromStack = int(moveCommand[3])-1
     toStack = int(moveCommand[5])-1
 
     for x in range(amount):
-        crane = column[fromStack].pop()
-        column[toStack].append(crane)
+        crane.append(column[fromStack].pop())
+    for x in range(amount):
+        # crane.append(column[fromStack].pop())
+        column[toStack].append(crane.pop())
+        # column[toStack].append(column[fromStack].pop())
+
     return (column)
 
 
@@ -38,31 +30,28 @@ def main():
     row = [] * 200
     column = [[] for x in range(9)]
 
-    rownum = 0
-
     with open('day-5.input') as f:
         lines = f.readlines()
     input = tuple(line.replace('\n', ' ') for line in lines)
 
     for li in input:
         # print(li)
-        if re.match('move', li):
+        if re.match('move', li):   # Movement Lines
             data = doMovement(li, column)
             printTable2(data)
-        else:
-            if re.match('^\s+1\s+', li):
-                for x in range(len(row)):
-                    #print(row[x])
-                    for y in range(9):
-                        #jprint(row[x][y])
-                        if re.match('^\[', (row[x][y])):
-                            column[y].append(row[x][y])
-                                    #print('asdfqasdfasdf', column[8])
-            elif re.match('^\[', li):
-                row.insert(0, [li[i:i+4] for i in range(0, len(li), 4)])
-                rownum += 1
+        elif re.match('^\s+1\s+', li):
+            # At the end turn the columns into arrays
+            for x in range(len(row)):
+                for y in range(9):  # 9 columns static
+                    # Doesn't add the blank spaces to the lists
+                    if re.match('^\[', (row[x][y])):
+                        column[y].append(row[x][y])
+        elif re.match('^\[', li):
+            row.insert(0, [li[i:i+4] for i in range(0, len(li), 4)])
 
+    print("Answer:")
     for x in range(len(data)):
-        print(data[x][-1])
+        print(f"{x} - {data[x][-1]}")
+
 
 main()
